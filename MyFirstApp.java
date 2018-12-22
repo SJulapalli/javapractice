1bc21db2e7b2fabf84f72b3a3dc16c4e8c440639
SJulapalli <46074052+SJulapalli@users.noreply.github.com>
2018-12-22 13:58:07 -0500

    MyFirstApp

---
+++ MyFirstApp.java
@@ -0,0 +1,24 @@
+import java.lang.Math;
+
+public class MyFirstApp {
+
+	public static void main (String[] args) {
+		System.out.println("Dad is a babo");
+
+			
+		double y = 7;
+		
+		
+		// Hello this is a comment
+
+
+		while (y != 10) {
+			//Thread.sleep(1000);
+			y = y * Math.pow(2, 4);
+			System.out.println(" y= " + y );
+			
+		}
+
+	}
+
+}
